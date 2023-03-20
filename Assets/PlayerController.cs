64daf14a226809e727e4545a0743da869f841ff1
toyede <toyede@naver.com>
2023-03-20 18:15:30 +0900

    .

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
+    }
+    void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
